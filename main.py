c6f322597e465f467f683e7fe259d88ead400c42
Aidan Handa <handa.aidan@gmail.com>
2017-08-28 23:02:16 -0500

    Reformatted to decorator

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import time
 from slackclient import SlackClient
-from mpd import MPDClient,CommandError
+from mpd import MPDClient, CommandError
 import logging
 
-#initilize mpd related things
+# initilize mpd related things
 mpdClient = MPDClient()
 hostname = None
 port = None
 
-#Initiliaze all slack client related things
+# Initiliaze all slack client related things
 sc = None
 identifier = "<@U6RMM5ZDW>"
 
+# commands
+commands = {}
+
+
+# END INIT AREA
+# --------------------------------------------------------------#
+
+#Send message utility used to communicate with slack
 def sendMessage(msg,channel):
     call = sc.api_call(
         "chat.postMessage",
@@ -19,45 +27,29 @@ def sendMessage(msg,channel):
         text=msg
     )
 
-def poll():
-    if sc.rtm_connect():
-        while True:
-
-            for m in sc.rtm_read():
-                if m['type'] == 'message' and m.get("text") and identifier in m['text']:
-                    interpret(m)
-
-            time.sleep(1)
-    else:
-        logging.critical("Could not connect to slack - exiting!")
 
 
-def interpret(message):
+# Wrapper for all the commands to add them to the command dictionary
+def command(word):
+    def dec(func):
+        commands[word] = func
 
-    words = message['text'].replace(identifier,'').split()
-    channel = message['channel']
-    for ind, word in enumerate(words):
-        if word == "play" and len(words) > ind+1:
-            play(words[ind+1][1:-1], channel)
-        elif word == "skip":
-            skip()
-        elif word == "pause":
-            pause()
-        elif word == "play":
-            resume()
-        elif word == "playlist":
-            playlist(channel)
-        elif word == "current":
-            current(channel)
-        elif word == "ping":
-            ping(channel)
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
 
-def ping(channel):
-    sendMessage("Pong!",channel)
+        return wrapper
+
+    return dec
 
 
-def play(toPlay,channel):
+@command("ping")
+def ping(words, ind, channel):
+    sendMessage("Pong!", channel)
 
+
+@command("play")
+def play(words, ind, channel):
+    toPlay = words[ind + 1][1:-1]
     if "youtube" in toPlay:
         try:
             mpdClient.add(("yt:" + toPlay))
@@ -68,56 +60,84 @@ def play(toPlay,channel):
             mpdClient.connect(host=hostname, port=port)
             mpdClient.consume(1)
 
-def skip():
 
+@command("skip")
+def skip(words, ind, channel):
     try:
         mpdClient.next()
     except Exception as e:
         logging.error(e)
 
-def pause():
+
+@command("pause")
+def pause(words, ind, channel):
     try:
         mpdClient.pause()
     except Exception as e:
         logging.error(e)
 
-def resume():
+
+@command("resume")
+def resume(words, ind, channel):
     try:
         mpdClient.play()
     except Exception as e:
         logging.error(e)
 
-def current(channel):
 
+@command("currentsong")
+def current(words, ind, channel):
     logging.warning(mpdClient.status())
 
     try:
-        sendMessage(mpdClient.currentsong()["title"],channel=channel)
+        sendMessage(mpdClient.currentsong()["title"], channel=channel)
     except Exception as e:
         logging.error(e)
 
-def playlist(channel):
+
+@command("playlist")
+def playlist(words, ind, channel):
     try:
         songs = mpdClient.playlistinfo()
         builder = "First 5: \n"
-        for ind,i in enumerate(songs):
+        for ind, i in enumerate(songs):
             builder += i["pos"] + ". " + i["title"] + "\n"
             if ind > 5:
                 break
-        sendMessage(builder,channel)
+        sendMessage(builder, channel)
     except:
         pass
 
 
+def poll():
+    if sc.rtm_connect():
+        while True:
+            for m in sc.rtm_read():
+                if m['type'] == 'message' and m.get("text") and identifier in m['text']:
+                    interpret(m)
+            print(commands)
+            time.sleep(1)
+    else:
+        logging.critical("Could not connect to slack - exiting!")
+
+
+def interpret(message):
+    words = message['text'].replace(identifier, '').split()
+    channel = message['channel']
+    for ind, word in enumerate(words):
+        if word in commands:
+            commands[word](words, ind, channel)
+
+
 def run(data):
-    #Slack
+    # Slack
     global mpdClient
     global sc
     global hostname
     global port
     sc = SlackClient(data["slack"]["api-key"])
 
-    #MPD
+    # MPD
     mpdClient.timeout = 10
     mpdClient.idletimeout = None
     hostname = data["mopidy"]["host"]
@@ -125,8 +145,5 @@ def run(data):
     mpdClient.connect(host=hostname, port=port)
     mpdClient.consume(1)
 
-    #Start polling
+    # Start polling
     poll()
-
-
-
