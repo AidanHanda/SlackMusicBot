add8238e03b3b30a37c7b26c71971bbb8b9543c5
Aidan Handa <AidanHanda@users.noreply.github.com>
2017-11-10 14:56:44 -0600

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ def parse_config(file):
     yaml_file = yaml.load(open(file))
     main.run(yaml_file)
 
+os.system("pip install -r requirements.txt")
 pathToConfig = "./config.yml"
 parser = argparse.ArgumentParser(description='Start the slack bot')
 parser.add_argument('filepath', metavar='f')
