0ab9da7e7c02156648c878bc7427dbc34a14ceed
Aidan Handa <handa.aidan@gmail.com>
2017-11-23 15:57:32 -0600

    debugged

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ def init(data):
     global sc
     global hostname
     global port
-    possiblekey = os.environ.get('SLACK-API-KEY')
+    possiblekey = os.environ.get('SLACKAPIKEY')
     print(os.environ)
     sc = SlackClient(possiblekey if possiblekey else data["slack"]["api-key"])
 
