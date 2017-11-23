72879d0df36aa821edbf5a8ad685723e05ca604a
Aidan Handa <handa.aidan@gmail.com>
2017-11-23 00:25:40 -0600

    debugged

--- commands.py
+++ commands.py
@@ -2,6 +2,8 @@ import logging
 import subprocess
 import traceback
 
+import mopidycli
+
 # import alsaudio
 from core import command, sendMessage, sendPrivateMessage, addSong
 from settings import mpdClient, VERSION_STRING, song_master
@@ -78,6 +80,7 @@ def current(Request):
 @command("playlist")
 def playlist(Request):
     try:
+        print(mopidycli.tracklist())
         songs = mpdClient.playlistinfo()
         builder = "First 5: \n"
         for count, i in enumerate(songs):
