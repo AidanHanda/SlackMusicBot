d64966b786c1f15ab3c0f3ab0c2036898cdd9396
Aidan Handa <handa.aidan@gmail.com>
2017-11-24 00:56:15 -0600

    debugging

--- core.py
+++ core.py
@@ -68,7 +68,7 @@ def poll():
         while True:
             mpdClient.ping()
             for m in getSC().rtm_read():
-                print(m)
+                print(m.get("text"))
                 if m['type'] == 'message' and m.get("text") and identifier in m['text']:
                     interpret(m)
             time.sleep(1)
