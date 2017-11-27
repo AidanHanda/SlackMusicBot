cf2248c8aa59d43558fd63074d44f9a19659bbf4
Aidan Handa <handa.aidan@gmail.com>
2017-11-26 18:10:49 -0600

    Debugging

--- commands.py
+++ commands.py
@@ -57,6 +57,7 @@ def play(Request, toPlay=None):
             song = all[-1]
             sendMessage('"' + song['title'] + '"' + " -  added to playlist!", Request.channel)
             settings.redis_db.set(song['id'], Request.user)
+            print(Request.raw_message)
             print(song)
         except Exception as e:
             sendMessage(traceback.format_exc() + "Link: " + toPlay, Request.channel)
